cfa929e2343736a6eebae69da33b19d3f432c2cd
junjiez <junjiez@codeaurora.org>
2019-03-14 15:38:03 +0800

    SnapdraongCamera:Fix Burst FPS
    Check if it's in app-ZSL, then
    switch back to regular zsl preview
    to avoid Burst FPS drop in longshot
    
    Change-Id: I53a7d1bd1838c844842be0f302df7936ccce4223
    CRs-Fixed: 2383451

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -5510,6 +5510,8 @@ public class CaptureModule implements CameraModule, PhotoController,
 
     private void setRepeatingBurstForZSL(int id)
             throws CameraAccessException,IllegalStateException{
+        if (!mPostProcessor.isZSLEnabled())
+            return;
         List<CaptureRequest> requests =
                 new ArrayList<CaptureRequest>();
         CaptureRequest previewZslRequest = mPreviewRequestBuilder[id].build();
