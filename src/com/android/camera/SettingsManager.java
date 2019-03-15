2c40494ab9f83d53d4a60920aad6c272ced15b45
xianming wang <mingwax@codeaurora.org>
2019-03-15 10:53:59 +0800

    SnapdragonCamera: Set applyFlash before applyCommonSettings
    Adjust the location of applyFlash before applyCommonSettings.
    In order to set CONTROL_AE_MODE effective.
    
    CRs-Fixed: 2413357
    Change-Id: Ice565aac0fb698c05a4711baaeebf78e27c58e13

--- src/com/android/camera/SettingsManager.java
+++ src/com/android/camera/SettingsManager.java
@@ -1912,4 +1912,10 @@ private void filterHFROptions() {
         editor.commit();
     }
 
+    public boolean isDeveloperEnabled() {
+        SharedPreferences sp = mContext.getSharedPreferences(
+                ComboPreferences.getGlobalSharedPreferencesName(mContext),
+                Context.MODE_PRIVATE);
+        return sp.getBoolean(SettingsManager.KEY_DEVELOPER_MENU, false);
+    }
 }
