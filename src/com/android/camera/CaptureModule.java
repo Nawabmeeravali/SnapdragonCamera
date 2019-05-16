cd5dce8a1f18ac8809fe587e761f89f19c61c41e
zhuw <zhuw@codeaurora.org>
2019-05-16 10:29:33 +0800

    fix force close when click done after resume
    if IntentMode is not normal mode,
    in case of home/resume Message after capture picture
    then click done to attach picture
    APP need finish APP to avoid null data
    
    Change-Id: I24c4cd8660b59e7a90098732b5271837cb288bcf

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -2874,6 +2874,10 @@ public class CaptureModule implements CameraModule, PhotoController,
         setBokehModeVisible();
         mJpegImageData = null;
         closeVideoFileDescriptor();
+        if (mIntentMode != CaptureModule.INTENT_MODE_NORMAL) {
+            mActivity.setResultEx(Activity.RESULT_CANCELED, new Intent());
+            mActivity.finish();
+        }
     }
 
     @Override
